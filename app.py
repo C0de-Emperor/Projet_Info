e0869be839310b6f05b70e6308222a7f56d94505
Emperor <mttmallet@gmail.com>
2025-04-03 22:01:58 +0200

    bases

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, )
+
+
+
+
+@app.route('/')
+def index ():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
