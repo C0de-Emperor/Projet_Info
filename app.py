212965a695da5733b67fd91239c344603ed0120a
Emperor <mttmallet@gmail.com>
2025-04-05 15:12:35 +0200

    login page

--- app.py
+++ app.py
@@ -2,22 +2,25 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
+        username = request.form.get('databaseId')
         password = request.form.get('password')
         if username == "admin" and password == "secret":
-            return redirect(url_for('dashboard'))
+            return "vous êtes connecté"
+            #return redirect(url_for('dashboard'))
         else:
-            return render_template('index.html', error="Identifiants invalides.")
-    return render_template('index.html')
+            return render_template('login.html', error="Identifiants invalides.")
+    return render_template('login.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return "Bienvenue dans le tableau de bord ! 🎉"
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
