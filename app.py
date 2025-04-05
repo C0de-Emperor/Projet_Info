f7d7a02e2421cc516d4ef53f8237f602b0886d9f
Emperor <mttmallet@gmail.com>
2025-04-05 12:22:52 +0200

    debut submit

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, url_for
-import dbManager
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index ():
+    print(request)
+    if request.method == 'POST':
+        taskContent = request.form['content']
+        return taskContent
+    
     return render_template("index.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
