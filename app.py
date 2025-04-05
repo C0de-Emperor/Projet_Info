5bfcf97f6f893c575e3426a5dc382e6a8f3a6099
ggglllkkk <emohu@outlook.fr>
2025-04-05 20:00:20 +0200

    kaka (matteo) -> better (mimile)

--- app.py
+++ app.py
@@ -32,37 +32,15 @@ def orgaLogin():
 @app.route('/createTournament', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        sport = request.form.get('sport')
-        matchDuration = request.form.get('matchDuration')
-        teamSize = request.form.get('teamSize')
-        availableSportsFields = request.form.get('availableSportsFields')
-        algorithm = request.form.get('algorithm')
-        maxTeamNumber = request.form.get('maxTeamNumber')
-        teamSelectionMethod = request.form.get('teamSelectionMethod')
-        points = request.form.get('points')
-        tournamentId = request.form.get('tournamentId')
-        password = request.form.get('password')
+        tournamentDict = {}
+        inputsNameList = ["sport", "matchDuration", "teamSize", "availableSportFields", "algorithm", "maxTeamNumbber", "teamSelectionMethod", "points", "tournamentId", "password"]
 
-        if sport == "" :
-            return render_template('create.html', error="Sport is empty")
-        if matchDuration == "" :
-            return render_template('create.html', error="Match Duration is empty")
-        if teamSize == "" :
-            return render_template('create.html', error="Team Size is empty")
-        if availableSportsFields == "" :
-            return render_template('create.html', error="Available Sports Fields is empty")
-        if algorithm == "" :
-            return render_template('create.html', error="Algorithm is empty")
-        if maxTeamNumber == "" :
-            return render_template('create.html', error="Max Team Number is empty")
-        if teamSelectionMethod == "" :
-            return render_template('create.html', error="Team Selection Method is empty")
-        if points == "" :
-            return render_template('create.html', error="Points Sports Fields is empty")
-        if tournamentId == "" :
-            return render_template('create.html', error="Tournament Id is empty")
-        if password == "" :
-            return render_template('create.html', error="Password is empty")
+        for k in inputsNameList:
+            tournamentDict[k] = request.form.get(k)
+
+        for (key, value) in tournamentDict:
+            if value == "":
+                return render_template("createTournament.html", error=key+" is empty")
         
         try:
             int(matchDuration)
